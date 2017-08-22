9f4bee40f7c0c31821e7c9f8c30e11a70f4ae14c
Bobby Allen <ballen@bobbyallen.me>
2017-08-22 09:41:55 +0100

    Adding TestCase use and class extension.

---
+++ tests/GPIOTest.php
@@ -0,0 +1,17 @@
+<?php
+use PHPUnit\Framework\TestCase;
+use Ballen\GPIO\GPIO;
+
+/**
+ * GPIO
+ * A RaspberryPi GPIO library written in PHP.
+ *
+ * @author Bobby Allen <ballen@bobbyallen.me>
+ * @license http://www.gnu.org/licenses/gpl-3.0.html
+ * @link https://github.com/allebb/gpio
+ * @link http://www.bobbyallen.me
+ */
+class GPIOTest extends TestCase
+{
+
+}
